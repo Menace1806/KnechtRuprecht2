c6320a3de6847ac39c09216e77992de8f22b1fb3
Dennis <Dennis.Kubanke@googlemail.com>
2016-11-04 15:34:41 +0100

    test

--- KnechtRuprecht/Splash.vb
+++ KnechtRuprecht/Splash.vb
@@ -7,22 +7,22 @@ Public NotInheritable Class Splash
 
         Show()
 
-        If My.Computer.Network.Ping("192.168.1.42", 5000) = True Then
-            System.Threading.Thread.Sleep(2000)
+        'If My.Computer.Network.Ping("192.168.1.42", 5000) = True Then
+        '    System.Threading.Thread.Sleep(2000)
 
-            Dim cc As New KnechtRuprecht.CallCenter
+        Dim cc As New KnechtRuprecht.CallCenter
             cc.Show()
             Close()
-        ElseIf My.Computer.Network.Ping("192.168.1.42", 5000) = True Then
-            System.Threading.Thread.Sleep(2000)
+        'ElseIf My.Computer.Network.Ping("192.168.1.42", 5000) = True Then
+        '    System.Threading.Thread.Sleep(2000)
 
-            Dim cc As New KnechtRuprecht.CallCenter
-            cc.Show()
-            Close()
-        Else
-            MsgBox("Datenbank nicht erreichbar")
-            Close()
-        End If
+        '    Dim cc As New KnechtRuprecht.CallCenter
+        '    cc.Show()
+        '    Close()
+        'Else
+        '    MsgBox("Datenbank nicht erreichbar")
+        '    Close()
+        'End If
 
 
 
